16a646b21741032c8766f0875f1236bf1ed8656d
Žilvinas Rudžionis <3447630+zrudzionis@users.noreply.github.com>
2019-02-09 13:13:13 +0200

    First failing test

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -1,1 +1,16 @@
 
+"""
+$5 + 10 CHF = $10 if rate is 2:1
+$5 * 2 = $10
+"""
+
+from unittest import TestCase
+
+
+class MoneyTestCase(TestCase):
+    def test_multiplication(self):
+        five = Dollar(5)
+        five.times(2)
+        self.assertEqual(five.amount, 10)
+
+
