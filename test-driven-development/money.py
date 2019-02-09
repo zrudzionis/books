45318cb192345986b7d400ec84598f1121d0dbc8
zilvarudzionis <zilvinas.rudzionis@revelsystems.com>
2019-02-09 14:14:12 +0200

    Make equality test red

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -33,3 +33,4 @@ class MoneyTestCase(TestCase):
     def test_equality(self):
         self.assertTrue(Dollar(5) == Dollar(5))
         self.assertFalse(Dollar(5) == Dollar(6))
+        self.assertFalse(Dollar(5) == dict())
