6b666dd7f702a91a552af6d7bf72763cf7986ef8
Žilvinas Rudžionis <3447630+zrudzionis@users.noreply.github.com>
2019-02-09 13:19:12 +0200

    Make test_multiplication green

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -9,10 +9,16 @@ Money rounding?
 from unittest import TestCase
 
 
+class Dollar(object):
+    def __init__(self, amount):
+        self.amount = 10
+
+    def times(self, multiplier):
+        pass
+
+
 class MoneyTestCase(TestCase):
     def test_multiplication(self):
         five = Dollar(5)
         five.times(2)
         self.assertEqual(five.amount, 10)
-
-
