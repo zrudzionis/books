4a0b0771a3539e3cc94d7d9284bbe34ed888c2b0
zilvarudzionis <zilvinas.rudzionis@revelsystems.com>
2019-02-09 14:49:31 +0200

    Introduce Franc tests

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -8,6 +8,7 @@ Money rounding?
 hashCode()
 equal None
 equal other object
+5 CHF * 2 = 10 CHF
 """
 
 from unittest import TestCase
@@ -29,6 +30,11 @@ class MoneyTestCase(TestCase):
         five = Dollar(5)
         self.assertEqual(five.times(2), Dollar(10))
         self.assertEqual(five.times(3), Dollar(15))
+
+    def test_franc_multiplication(self):
+        five = Franc(5)
+        self.assertEqual(five.times(2), Franc(10))
+        self.assertEqual(five.times(3), Franc(15))
 
     def test_equality(self):
         self.assertTrue(Dollar(5) == Dollar(5))
