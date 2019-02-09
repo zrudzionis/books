0b5f078a353c51a119c7dd6888ab1ef155f9d253
zilvarudzionis <zilvinas.rudzionis@revelsystems.com>
2019-02-09 13:43:41 +0200

    Remove side effects

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -2,7 +2,7 @@
 $5 + 10 CHF = $10 if rate is 2:1
     $5 * 2 = $10
 Make "amount" private
-Dollar side-effects?
+    Dollar side-effects?
 Money rounding?
 """
 
@@ -14,8 +14,7 @@ class Dollar(object):
         self.amount = amount
 
     def times(self, multiplier):
-        self.amount *= multiplier
-        return None
+        return Dollar(self.amount * multiplier)
 
 
 class MoneyTestCase(TestCase):
