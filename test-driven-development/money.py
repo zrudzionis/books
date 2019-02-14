307a6bc0acd8cd1beb3ab6117b1e8ff08cdf229d
zilvarudzionis <zilvinas.rudzionis@revelsystems.com>
2019-02-14 21:33:56 +0200

    Update todo

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -1,7 +1,10 @@
 """
 $5 + 10 CHF = $10 if rate is 2:1
-------------> $5 + $5 = $10
+$5 + $5 = $10
 Return Money from $5 + $5
+    Bank.reduce(Money)
+------------> Reduce Money with conversion
+Reduce(Bank, String)
 """
 
 from unittest import TestCase
