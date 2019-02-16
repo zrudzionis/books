b9a079fb0f66eabd1ce78770930f7acb7a210c35
zilvarudzionis <zilvinas.rudzionis@revelsystems.com>
2019-02-16 18:01:27 +0200

    Decide not to do testing of $5 + $5 = $10 since Money.plus returns sum

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -1,7 +1,7 @@
 """
     $5 + 10 CHF = $10 if rate is 2:1
     $5 + $5 = $10
-------------> Return Money from $5 + $5
+    Return Money from $5 + $5
     Bank.reduce(Money)
     Reduce Money with conversion
     Reduce(Bank, String)
