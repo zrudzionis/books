7cf24c30c60ed359a50bda33be25ff6f967d2dd2
zilvarudzionis <zilvinas.rudzionis@revelsystems.com>
2019-02-16 17:20:11 +0200

    Update todo list

--- test-driven-development/money.py
+++ test-driven-development/money.py
@@ -1,11 +1,11 @@
 """
-------------> $5 + 10 CHF = $10 if rate is 2:1
+    $5 + 10 CHF = $10 if rate is 2:1
     $5 + $5 = $10
 Return Money from $5 + $5
     Bank.reduce(Money)
     Reduce Money with conversion
     Reduce(Bank, String)
-Sum.plus
+------------> Sum.plus
 Expression.times
 """
 
